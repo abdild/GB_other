3f7d3531dbb1e1041d073eaf33c1e94aba275234
Abdullin_Ildar <abdullintat@yandex.ru>
2022-02-16 16:14:24 +0300

    lesson4

--- Patterns/lesson_4.php
+++ Patterns/lesson_4.php
@@ -7,3 +7,22 @@
 // DBQueryBuiler — конструктор запросов к базе.
 // Должна получиться гибкая система, позволяющая динамически менять базу данных и инкапсулирующая взаимодействие с ней внутри продуктов конкретных фабрик. Углубляться в детали компонента не обязательно — достаточно их наличия.
 
+abstract class DBFactory {
+
+}
+
+interface DBConnection {}
+interface DBRecord {}
+interface DBQueryBuilder {}
+
+class MySQLDBConnection implements DBConnection {}
+class PostgreSQLConnection implements DBConnection {}
+class OracleConnection implements DBConnection {}
+
+class MySQLDBRecord implements DBRecord {}
+class PostgreSQLRecord implements DBRecord {}
+class OracleRecord implements DBRecord {}
+
+class MySQLDBQueryBuilder implements DBQueryBuilder {}
+class PostgreSQLQueryBuilder implements DBQueryBuilder {}
+class OracleQueryBuilder implements DBQueryBuilder {}
